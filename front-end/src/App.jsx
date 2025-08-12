73af3a3833347a6223363f4e3d65e07225ec9891
nikoladev <nikoladev06@gmail.com>
2025-08-12 14:29:30 -0300

    Começando a criar o cabeçalho

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,18 @@
+import Header from "./components/header";
+
 function App() {
-  return <div className="text-3xl font-bold text-gray-600 underline">App</div>;
+  return (
+    <div>
+      <div className="flex items-center">
+        <img
+          className="h-10"
+          src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
+          alt="Logo da Hashtag"
+        />
+        <p>ashbnb</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
