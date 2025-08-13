8e935ca318c3853ca098dbbca2fd91c70d5fa1b7
nikoladev06 <nikoladev06@gmail.com>
2025-08-12 22:47:47 -0300

    feat: Conclusão da adição do cabeçalho

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,7 @@
 import Header from "./components/header";
 
 function App() {
-  return (
-    <div>
-      <div className="flex items-center">
-        <img
-          className="h-10"
-          src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
-          alt="Logo da Hashtag"
-        />
-        <p>ashbnb</p>
-      </div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
